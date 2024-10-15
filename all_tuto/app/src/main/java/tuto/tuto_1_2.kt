c1235c22b2e0ddebd32edc3bb3a5aaf5e33ed9f9
Ayoub <ayoubsolicode07@gmail.com>
2024-10-15 13:05:38 +0100

    add tuto 1.2

---
+++ all_tuto/app/src/main/java/tuto/tuto_1_2.kt
@@ -0,0 +1,28 @@
+package tuto
+
+fun main (){
+        val kotlin = null
+        println(kotlin)
+
+//        var FavoriteActor: String = "Hello"
+//        FavoriteActor = "welcome"
+//        println(FavoriteActor)
+//    Outpout welcome cuz var can be reassigne
+
+//    fun main() {
+//    var FavoriteActor: String? = "Hello"
+//    println(FavoriteActor)
+//    FavoriteActor = null
+//    println(FavoriteActor)
+//}
+//        fun main() {
+//    var FavoriteActor: String? = null
+//    println(FavoriteActor?.length)
+//
+//}
+//        fun main() {
+//    var favoriteActor: String? = null
+//	val lengthOfName = favoriteActor?.length ?: 1
+//    println("the number is $lengthOfName")
+//}
+}
